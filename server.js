c69b9d688cdee3a513809474354165a6785f3b61
dhaval <dhaval.shah@cygnetinfotech.com>
2020-01-22 14:10:42 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var hbs = require('hbs');
+var app     = express();
+var fs = require('fs');
+
+
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('curentYear', () => {
+    return (new Date()).getFullYear()
+});
+
+app.use((req, res, next) => {
+    var log = `${(new Date()).toString()} ${req.method} ${req.url} \n`;
+    console.log(log);
+    fs.appendFile('server.log', log, (err) => {
+        if(err)
+        {
+            console.log(err)
+        }
+    })
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintainance.hbs');
+});
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, resp)
+{
+    resp.render('home.hbs',
+    {
+        title: 'Welcome to hbs'
+    })
+});
+
+app.get('/about', function(req, resp)
+{
+    resp.render('about.hbs',
+    {
+        title: 'Welcome to hbs'
+    })
+});
+
+app.listen(3000);
